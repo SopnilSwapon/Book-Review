a0390932ea4345c40f4075ce8c22d71b6b906610
Sopnil Swapon <sopnilswapon1@gmail.com>
2024-03-25 23:25:11 +0600

    display all book

--- src/Components/Books/Books.jsx
+++ src/Components/Books/Books.jsx
@@ -12,9 +12,11 @@ const Books = () => {
     return (
         <div>
             <h2 className="text-3xl font-bold text-center mt-10">Books {books.length}</h2>
+            <div className="grid grid-cols-1  md:grid-cols-3 gap-16">
             {
                 books.map(book => <Book key={book.bookId} book={book}></Book>)
             }
+            </div>
         </div>
     );
 };
